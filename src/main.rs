2f5ee7930a6ca24a9e7a64669fc76714c1bfbbb7
Nola G <gingerwitch64@gmail.com>
2024-07-29 14:27:59 -0700

    Swap out if statement for match statement

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ fn main() {
     let arg: Vec<String> = env::args().collect();
     let filehash: String;
 
-    if arg[1] == "sha256".to_string() {
-        let infile = Path::new(arg[2].as_str());
-        filehash = sha256::try_digest(infile).unwrap();
-    } else { println!("Something went wrong..."); exit(2) }
+    match arg[1].to_lowercase().as_str() {
+        "sha256" => {
+            let infile = Path::new(arg[2].as_str());
+            filehash = sha256::try_digest(infile).unwrap(); },
+        _ => { println!("Error: {} is not a supported algorithm...", arg[1]); exit(2); },
+    }
     
     if arg[3].to_lowercase() == filehash {
         println!("The hash of {} matches!", arg[1]);
